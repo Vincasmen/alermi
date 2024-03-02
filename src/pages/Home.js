bd073106dda66acf2b7bdffad8ab7bd9d45a54fc
Vincasmen <vincasmen13@gmail.com>
2024-03-02 21:38:37 +0100

    Added outlet to Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Outlet />
     </div>
   );
 };
