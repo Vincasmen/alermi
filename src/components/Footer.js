051e76323ed8e0070ddfd517b0e6ad8aa7318a9d
Vincasmen <vincasmen13@gmail.com>
2024-03-04 20:48:41 +0100

    Deleted icons and added some info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,37 +14,27 @@ export default function App() {
           {/* <!-- TW Elements section --> */}
           <div className="">
             <h6 className="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="mr-3 h-4 w-4"
-              >
-                <path d="M12.378 1.602a.75.75 0 00-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03zM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 00.372-.648V7.93zM11.25 22.18v-9l-9-5.25v8.57a.75.75 0 00.372.648l8.628 5.033z" />
-              </svg>
-              TW Elements
+              Elektroniceké recepty
             </h6>
-            <p>
-              Here you can use rows and columns to organize your footer content.
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            </p>
+            <p>SMS žádost: 605 270 039</p>
+            <p>e-mail: alergologie@alermi.cz</p>
+            <h6 className="mb-4 pt-3 flex items-center justify-center font-semibold uppercase md:justify-start">
+              Kontakt na lékaře
+            </h6>
+            <p>minarikova@alermi.cz</p>
           </div>
           {/* <!-- Products section --> */}
           <div className="">
             <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-              Products
+              Jak se k nám dostanete
             </h6>
             <p className="mb-4">
-              <a className="text-neutral-600 dark:text-neutral-200">Angular</a>
-            </p>
-            <p className="mb-4">
-              <a className="text-neutral-600 dark:text-neutral-200">React</a>
-            </p>
-            <p className="mb-4">
-              <a className="text-neutral-600 dark:text-neutral-200">Vue</a>
-            </p>
-            <p>
-              <a className="text-neutral-600 dark:text-neutral-200">Laravel</a>
+              <a className="text-neutral-600 dark:text-neutral-200">
+                Zastávka tramvají č. 3 , 5 , 9 , 11 - zastávka Jugoslávská přímo
+                u Domu zdraví
+                <br />
+                Parkování osobních vozidel je možné na parkovišti uvnitř areálu.
+              </a>
             </p>
           </div>
           {/* <!-- Useful links section --> */}
