6e156e93c92e27ca99cce9ef196de194aca5465b
Vincasmen <vincasmen13@gmail.com>
2024-03-05 19:01:55 +0100

    Added shared layout for nav and footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import SharedLayout from "./pages/SharedLayout";
 import "./App.css";
 
 function App() {
@@ -7,7 +8,9 @@ function App() {
     <BrowserRouter>
       {" "}
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,7 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 
 const Home = () => {
-  return (
-    <div>
-      <Navbar />
-      <Footer />
-      <Outlet />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
