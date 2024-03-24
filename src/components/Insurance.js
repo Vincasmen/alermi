20adc1d88d59267b44f2023d3f91f1673102441f
Vincasmen <vincasmen13@gmail.com>
2024-03-24 23:32:11 +0100

    Createc component for insurance companies logos

---
+++ src/components/Insurance.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Insurance = () => {
+  return (
+    <div className="insurance-container 2xl:container 2xl:mx-auto lg:pt-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
+      <div></div>
+    </div>
+  );
+};
+
+export default Insurance;
