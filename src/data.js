c00420ce1d251414bb1e824bbbc63ad365f3397d
Vincasmen <vincasmen13@gmail.com>
2024-03-26 21:23:41 +0100

    Created data.js for insurance companies data

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const books = [
+  {
+    id: 1,
+    image: "",
+    title: "",
+  },
+  {
+    id: 2,
+    image: "",
+    title: "",
+  },
+  {
+    id: 3,
+    image: "",
+    title: "",
+  },
+  {
+    id: 4,
+    image: "",
+    title: "",
+  },
+  {
+    id: 5,
+    image: "",
+    title: "",
+  },
+  {
+    id: 6,
+    image: "",
+    title: "",
+  },
+];
+
+export default books;
