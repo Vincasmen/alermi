e4ddec7b6d62235268b740545b3b900c36ffad0f
Vincasmen <vincasmen13@gmail.com>
2024-04-02 18:26:27 +0200

    Created the FAQ component and filled it with data from FAQdata.js

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import FAQdata from "../dataFAQ";
 
 const FAQ = () => {
+  const [activeQuestion, setActiveQuestion] = useState(null);
+  const answerRef = useRef(null);
+
+  const toggleQuestion = (index) => {
+    setActiveQuestion(activeQuestion === index ? null : index);
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      // Zavřít aktivní otázku, pokud uživatel klikne mimo pole otázek a mimo pole s odpovědí
+      if (
+        !event.target.closest(".faq-question") &&
+        !event.target.closest(".faq-answer")
+      ) {
+        setActiveQuestion(null);
+      }
+    };
+
+    // Přidat posluchač událostí po načtení komponenty
+    document.body.addEventListener("click", handleOutsideClick);
+
+    // Odebrat posluchač událostí po odmontování komponenty
+    return () => {
+      document.body.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
+
   return (
     <div className="font-action hero-container relative m-auto flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0 w-full lg:max-w-[1240px]">
-      <div></div>
+      <div>
+        {FAQdata.map((faqData, index) => (
+          <div key={index} className="shadow-lg">
+            <h1
+              className="font-bold text-lg cursor-pointer faq-question"
+              onClick={() => toggleQuestion(index)}
+            >
+              {faqData.question}
+            </h1>
+            {activeQuestion === index && (
+              <div ref={answerRef} className="mt-2 faq-answer">
+                <ul>
+                  <li>{faqData.answer}</li>
+                </ul>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/dataFAQ.js
+++ src/dataFAQ.js
@@ -2,12 +2,39 @@ const insurance = [
   {
     id: 1,
     question: "První vyšetření",
-    answer: <p></p>,
+    answer: (
+      <div>
+        <p>- kartičku zdravotní pojišťovny</p> <br />
+        <p>- doporučení odesílajícího lékaře</p> <br />
+        <p>
+          - výsledky laboratorního vyšetření a nálezy z provedených vyšetření v
+          poslední době
+        </p>
+      </div>
+    ),
   },
   {
     id: 2,
     question: "Kde nás najdete?",
-    answer: "Revírní bratrská pokladna",
+    answer: (
+      <div>
+        <p>
+          Zastávka tramvají č. 3 , 5 , 9 , 11 - zastávka Jugoslávská přímo u
+          Domu zdraví Parkování osobních vozidel je možné na parkovišti uvnitř
+          areálu.
+        </p>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2606.808470801625!2d16.622897535443126!3d49.20419281690067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471294f54a163ad5%3A0x4dd05185757b7e6a!2zRMWvbSB6ZHJhdsOtIE1hcnR5IEhhcnRsb3bDqQ!5e0!3m2!1scs!2scz!4v1712073453106!5m2!1scs!2scz"
+          width="600"
+          height="450"
+          style={{ color: "black" }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+          title="map"
+        ></iframe>
+      </div>
+    ),
   },
 ];
 
