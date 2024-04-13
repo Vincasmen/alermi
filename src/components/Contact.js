78fb6de1feadf12938c1bf23142ca098f620a7c7
Vincasmen <vincasmen13@gmail.com>
2024-04-13 12:04:39 +0200

    Added form section to Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section>
+      <div className="">
+        <h1 className="text-center mb-5 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+          Napište nám
+        </h1>
+        <form className="flex flex-row">
+          <div>
+            <label>Jméno a příjmení*</label>
+            <input
+              className="bg-slate-100"
+              placeholder="Povinné"
+              type="text"
+              maxLength="255"
+            />
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -38,14 +38,14 @@ const Hero = () => {
             <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
               Alermi
             </p>
-            <h2 className=" mb-5 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+            <h1 className=" mb-5 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
               Vítejte na stránkách
               <br />
               alergologické ordinace{" "}
               <span className="pulsate-fwd-4 inline-block text-deep-purple-accent-400 bg-clip-text hover:text-transparent bg-gradient-to-br from-[#00df9a] to-[#86efac]  transition ease-in-out duration-500">
                 Alermi Brno
               </span>
-            </h2>
+            </h1>
             <p className="font-display pr-5 mb-5 text-base text-gray-700 md:text-lg">
               S naším týmem alergologů a moderní diagnostikou vám pomůžeme
               identifikovat příčiny vašich obtíží a navrhnout efektivní léčbu.
