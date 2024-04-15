b73233bf3930ecff5630ca63c9d4d0053f43f6bd
Vincasmen <vincasmen13@gmail.com>
2024-04-15 18:22:25 +0200

    Added heading to Insurance component

--- src/components/Insurance.js
+++ src/components/Insurance.js
@@ -4,6 +4,9 @@ import Data from "../data"; // Importujeme komponentu Data
 const Insurance = () => {
   return (
     <div className="font-display container mx-auto py-9 px-4 ">
+      <h1 className="mb-5 text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+        Smluvní pojišťovny
+      </h1>
       <div className="grid grid-cols-1 m-auto  sm:w-auto sm:m-0 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {/* Mapování dat z komponenty Data */}
         {Data.map((insuranceItem) => (
